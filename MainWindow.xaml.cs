ed4dabe249d6b7f34171895b754aa47ac7ce5bb5
Petrik Diak <diak@petrik.hu>
2025-11-25 13:57:29 +0100

    olvaso, fajl beolvasás, kész profi

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,12 +12,34 @@ using System.Windows.Shapes;
 namespace KonyvtarAvatkozas
 {
 
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-            //kezdes
-        }
-    }
+	public partial class MainWindow : Window
+	{
+		const string FILENAME = "olvasok.txt";
+		List<Olvaso> olvasok = new List<Olvaso>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			BetoltOlvasok();
+		}
+
+		private void BetoltOlvasok()
+		{
+			if (File.Exists(FILENAME))
+			{
+				foreach (var line in File.ReadAllLines(FILENAME, Encoding.UTF8))
+				{
+					if (!string.IsNullOrWhiteSpace(line))
+					{
+						try
+						{
+							olvasok.Add(Olvaso.Parse(line));
+						}
+						catch { /* hibás sor kihagyása */ }
+					}
+				}
+				FrissitListBox();
+			}
+		}
+	}
 }
