c04e0b4477c9533f51e04d3a8bf29dc2d65fdb9d
Petrik Diak <diak@petrik.hu>
2025-11-25 14:02:04 +0100

    cs fájl létrehozva

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -28,11 +31,71 @@ namespace KonyvtarAvatkozas
 						{
 							olvasok.Add(Olvaso.Parse(line));
 						}
-						catch { /* hibás sor kihagyása */ }
+						catch { }
 					}
 				}
 				FrissitListBox();
 			}
+		}
+
+		private void FrissitListBox()
+		{
+			olvasokListBox.ItemsSource = null;
+			olvasokListBox.ItemsSource = olvasok.ConvertAll(o => o.Nev);
+		}
+
+		private void EletkorTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = !int.TryParse(e.Text, out _);
+		}
+
+		private void mentesButton_Click(object sender, RoutedEventArgs e)
+		{
+			visszajelzesTextBlock.Text = "";
+
+			if (string.IsNullOrWhiteSpace(nevTextBox.Text) || string.IsNullOrWhiteSpace(eletkorTextBox.Text))
+			{
+				visszajelzesTextBlock.Text = "Kérem, töltse ki a kötelező mezőket!";
+				visszajelzesTextBlock.Foreground = System.Windows.Media.Brushes.Red;
+				return;
+			}
+
+			if (!int.TryParse(eletkorTextBox.Text, out int eletkor))
+			{
+				visszajelzesTextBlock.Text = "Életkor csak szám lehet!";
+				visszajelzesTextBlock.Foreground = System.Windows.Media.Brushes.Red;
+				return;
+			}
+
+			string mufaj = "";
+			if (mufajComboBox.SelectedItem is ComboBoxItem cbi)
+				mufaj = cbi.Content.ToString();
+
+			string ertesites = "";
+			if (hirlevelCheckBox.IsChecked == true) ertesites += "Hírlevél;";
+			if (smsCheckBox.IsChecked == true) ertesites += "SMS;";
+			if (ertesites.EndsWith(";")) ertesites = ertesites.Substring(0, ertesites.Length - 1);
+			if (string.IsNullOrWhiteSpace(ertesites)) ertesites = "nincs";
+
+			string tagsag = normalRadio.IsChecked == true ? "Normál" :
+							diakRadio.IsChecked == true ? "Diák" : "Nyugdíjas";
+
+			var ujOlvaso = new Olvaso
+			{
+				Nev = nevTextBox.Text,
+				Eletkor = eletkor,
+				Mufaj = mufaj,
+				Ertesitesek = ertesites,
+				Tagsag = tagsag
+			};
+
+			olvasok.Add(ujOlvaso);
+
+			File.AppendAllText(FILENAME, ujOlvaso + Environment.NewLine, Encoding.UTF8);
+			FrissitListBox();
+
+			visszajelzesTextBlock.Text = "Sikeres regisztráció!";
+			visszajelzesTextBlock.Foreground = System.Windows.Media.Brushes.Green;
 		}
 	}
 }
---
+++ Olvaso.cs
@@ -0,0 +1,29 @@
+﻿namespace KonyvtarAvatkozas
+{
+	public class Olvaso
+	{
+		public string Nev { get; set; }
+		public int Eletkor { get; set; }
+		public string Mufaj { get; set; }
+		public string Ertesitesek { get; set; }
+		public string Tagsag { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Nev}|{Eletkor}|{Mufaj}|{Ertesitesek}|{Tagsag}";
+		}
+
+		public static Olvaso Parse(string line)
+		{
+			var parts = line.Split('|');
+			return new Olvaso
+			{
+				Nev = parts[0],
+				Eletkor = int.Parse(parts[1]),
+				Mufaj = parts[2],
+				Ertesitesek = parts[3],
+				Tagsag = parts[4]
+			};
+		}
+	}
+}
